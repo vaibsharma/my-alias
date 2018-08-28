fb6498cf07881e69abf0e4c9ae00627cb49d86b0
Vaibhav Sharma <vaib.sharma44@gmail.com>
2018-08-29 00:36:25 +0530

    added my system aliases for daily usage

---
+++ my-aliases.sh
@@ -0,0 +1,20 @@
+VAIBHAV="/home/vaibahv"
+
+alias activate="source env/bin/activate"
+alias aliases="vi $VAIBHAV/.my-aliases/my-aliases.sh"
+alias codejam="cd $VAIBHAV/vaibhav/codejam"
+alias codeforces="cd /$VAIBHAV/vaibhav/codeforces"
+alias cpp="cp $VAIBHAV/.my-allias/templates/c++_template.cpp ./a.cpp && mv ./a.cpp"   #example cpp first.cpp
+
+alias interview="cd $VAIBHAV/vaibhav/technicalInterview"
+
+alias subln="$VAIBHAV/.sublime3/sublime_text_3/sublime_text"
+
+alias practice="cd $VAIBHAV/vaibhav/algorithmscpp"
+
+alias update="sudo apt-get update"
+
+alias venv="virtualenv env"
+alias venv3="virtualenv -p python3 env"
+
+alias whatsapp="$VAIBHAV/Downloads/temp/whatsapp/Whatsapp-Desktop-0.5.0/dist/WhatsApp-linux-x64/WhatsApp" 
---
+++ templates/c++_template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct sp{
+	int f;
+	int s;
+	int cost;
+	sp(){
+		f = 0;
+		s = 0;
+		cost = 0;
+	}
+	bool check(sp a, sp b){
+		return a.f < b.f;
+	}
+};
+
+int main(){
+	int T=0;
+	cin>>T;
+	for(int x=0;x<T;x++){
+	}
+	return 0;
+}
